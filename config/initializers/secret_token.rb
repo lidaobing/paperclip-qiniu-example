19b8b7bacdb9e488b9ad1e141493ff449566c62d
LI Daobing <lidaobing@gmail.com>
2012-07-13 21:21:25 +0800

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperclipQiniuExample::Application.config.secret_token = 'b2dbe2098623b0c833a27d6fda6a069041f9953ef7904c4223fa54c7bd66b11c3f3885516550ba3da9a3e0af5655a204df4edd49131c4bb5f97b274f7eaf6713'
